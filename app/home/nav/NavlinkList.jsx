0324a5eeb0df08a0498c0adcc514f684ce795b8c
Chris Awoke <uchechristian27@gmail.com>
2023-12-25 15:18:18 +0100

    new

--- app/home/nav/NavlinkList.jsx
+++ app/home/nav/NavlinkList.jsx
@@ -1,47 +1,46 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import Link from "next/link";
+import React, { useRef, useState } from "react";
 import { FaAngleDown } from "react-icons/fa6";
 import { NavListData } from "@/constants";
 
 const NavlinkList = () => {
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [icon, setIcon] = useState(null);
-  const [sublinks, setsublinks] = useState(false);
-  const [visible, setVisible] = useState(false);
-  const closeMenu = useRef()
+  // const [selectedItem, setSelectedItem] = useState(null);
+  // const [icon, setIcon] = useState(null);
+  // const [visible, setVisible] = useState(false);
+  // const closeMenu = useRef();
 
   //function to set the id of drowdown menu modal base on the id clicked or hover on
-  const toggle = (id) => {
-    setSelectedItem(id);
-    setIcon(id);
-    setsublinks(id);
-  };
+  // const toggle = (id) => {
+  //   setSelectedItem(id);
+  //   setIcon(id);
+  //   setsublinks(id);
+  // };
 
-  //show the dropdown menu modal
-  const show = () => {
-    setVisible((prev) => !prev);
-  };
+  // //show the dropdown menu modal
+  // const show = () => {
+  //   setVisible((prev) => !prev);
+  // };
 
+  // //Close menu by clicking outside
+  // useEffect(() => {
+  //   let handler = (event) => {
+  //     if (!closeMenu.current.contains(event.target)) {
+  //       setVisible(false);
+  //     }
+  //   };
+  //   document.body.addEventListener("mousedown", handler);
 
-  //Close menu by clicking outside
-  useEffect(() => {
-    let handler = (event) => {
-      if(!closeMenu.current.contains(event.target)){
-        setVisible(false);
-      }
-    }
-    document.body.addEventListener('mousedown', handler)
-
-    return () => {
-      document.body.removeEventListener('mousedown', handler)
-    }
-
-  })
+  //   return () => {
+  //     document.body.removeEventListener("mousedown", handler);
+  //   };
+  // });
 
   return (
-    <main className="flex flex-col md:flex-row item-start md:items-center" ref={closeMenu}>
+    <main
+      className="flex flex-col md:flex-row item-start md:items-center"
+      ref={closeMenu}
+    >
       {NavListData.map((link) => (
         <div key={link.id} onClick={() => toggle(link.id)}>
           <div className="px-3 md:flex text-center md:cursor-pointer group">
@@ -50,8 +49,7 @@ const NavlinkList = () => {
               onMouseOver={() => {
                 toggle(link.id);
                 show();
-              }
-              }
+              }}
               className={`${
                 selectedItem === link.id
                   ? "text-lightBlue"
@@ -60,7 +58,7 @@ const NavlinkList = () => {
                 visible ? "text-lightBlue" : "text-primaryBlack2"
               } px-3 py-5 md:py-2 flex justify-between items-center font-semibold text-sm md:pr-0 pr-5 group cursor-pointer hover:bg-gray-200 rounded`}
             >
-                {link.name}
+              {link.name}
               <span>
                 <FaAngleDown
                   className={`${
@@ -73,32 +71,6 @@ const NavlinkList = () => {
                 />
               </span>
             </h1>
-            <div>
-              {sublinks === link.id && visible ? (
-                <div>
-                  {link.submenu && (
-                    <div>
-                      <div className="md:ml-[-20px] md:absolute md:top-12 z-50 ">
-                        <div className="hidden md:block py-3">
-                          <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
-                        </div>
-                        <div className="md:bg-white py-5 md:w-[400px] md:h-[300px] md:shadow-xl">
-                          {link.sublink.map((links) => (
-                            <ul className="" key={links.name}>
-                              <li className="text-base text-gray-600 my-2.5 py-2 hover:bg-lightBlue  hover:text-white hover:text-semibold text-center">
-                                <Link href={links.link}>{links.name}</Link>
-                              </li>
-                            </ul>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              ) : (
-                <></>
-              )}
-            </div>
           </div>
         </div>
       ))}
--- constants/index.js
+++ constants/index.js
@@ -9,7 +9,6 @@ import image8 from "@/public/assets/images/LinkedIn.svg";
 import image9 from "@/public/assets/images/Instagram.svg";
 import image10 from "@/public/assets/images/email-icon.svg";
 import image11 from "@/public/assets/images/Mobile.svg";
-import image12 from "@/public/assets/images/Location.svg";
 import { IoMdMail } from "react-icons/io";
 import { FaSms, FaWpforms } from "react-icons/fa";
 
@@ -18,13 +17,6 @@ export const NavListData = [
     id: 1,
     name: "Features",
     submenu: true,
-    sublink: [
-      { name: "Audit", link: "/" },
-      { name: "Manage", link: "/" },
-      { name: "Secure", link: "/" },
-      { name: "Automate", link: "/" },
-      { name: "About", link: "/" },
-    ],
   },
   {
     id: 2,
@@ -62,7 +54,7 @@ export const NavListData = [
       { name: "About", link: "/" },
     ],
   },
-  
+
   {
     id: 6,
     name: "Contacts",
@@ -235,247 +227,297 @@ export const contacts = [
 
 // Hero section of email marketing page
 export const stats = [
-    {
-      id: 1,
-      title: "3 billion+",
-      details: "Calling numbers connected",
-    },
-    {
-      id: 2,
-      title: "100 billion+",
-      details: "Text messages sent",
-    },
-    {
-      id: 3,
-      title: "121 million+",
-      details: "phone numbers given out",
-    }
-  ];
-  
-  // Product section of email page
-  export const products = [
-    {
-      id: 1,
-      position: "relative",
-      variations: [
-        { bg: "#ffe6e6", top: "0", image: "/assets/icons/github.icon.svg", title: "3 billion+", details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atquecupiditate delectus est distinctio veritatis." },
-        { bg: "#e6f0ff", top: "2.5rem", image: "/assets/icons/github.icon.svg", title: "3 billion+", details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atquecupiditate est distinctio veritatis." },
-        { bg: "#e1f6f4", top: "5rem", image: "/assets/icons/github.icon.svg", title: "3 billion+", details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus est distinctio veritatis." },
-      ],
-    },
-    {
-      id: 2,
-      variations: [
-        { bg: "#ffe6ff", top: "0", image: "/assets/icons/github.icon.svg", title: "3 billion+ ", details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. delectus est distinctio veritatis." },
-        { bg: "#e6ffee", top: "2.5rem", image: "/assets/icons/github.icon.svg", title: "3 billion+", details: "Calling numbers connected" },
-        { bg: "#f0f0f5", top: "5rem", image: "/assets/icons/github.icon.svg", title: "3 billion+", details: "Calling numbers connected" },
-      ],
-    },
-  ];
-  
-  // Community section of Hero page
-  export const items = [
-    {
-      id: 1,
-      image: "/assets/icons/support.svg",
-      title: "Community Support",
-      description:
-        "Community Support is also available for all open source users.",
-    },
-    {
-      id: 2,
-      image: "/assets/icons/github.icon.svg",
-      title: "MailBlink on GitHub",
-      description: "Track the MailBlink development changes on GitHub.",
-    },
-  ];
+  {
+    id: 1,
+    title: "3 billion+",
+    details: "Calling numbers connected",
+  },
+  {
+    id: 2,
+    title: "100 billion+",
+    details: "Text messages sent",
+  },
+  {
+    id: 3,
+    title: "121 million+",
+    details: "phone numbers given out",
+  },
+];
 
-  // Regex for email validation
-  export const emailRegex = {
-    pattern:
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  };
+// Product section of email page
+export const products = [
+  {
+    id: 1,
+    position: "relative",
+    variations: [
+      {
+        bg: "#ffe6e6",
+        top: "0",
+        image: "/assets/icons/github.icon.svg",
+        title: "3 billion+",
+        details:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atquecupiditate delectus est distinctio veritatis.",
+      },
+      {
+        bg: "#e6f0ff",
+        top: "2.5rem",
+        image: "/assets/icons/github.icon.svg",
+        title: "3 billion+",
+        details:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atquecupiditate est distinctio veritatis.",
+      },
+      {
+        bg: "#e1f6f4",
+        top: "5rem",
+        image: "/assets/icons/github.icon.svg",
+        title: "3 billion+",
+        details:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus est distinctio veritatis.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    variations: [
+      {
+        bg: "#ffe6ff",
+        top: "0",
+        image: "/assets/icons/github.icon.svg",
+        title: "3 billion+ ",
+        details:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. delectus est distinctio veritatis.",
+      },
+      {
+        bg: "#e6ffee",
+        top: "2.5rem",
+        image: "/assets/icons/github.icon.svg",
+        title: "3 billion+",
+        details: "Calling numbers connected",
+      },
+      {
+        bg: "#f0f0f5",
+        top: "5rem",
+        image: "/assets/icons/github.icon.svg",
+        title: "3 billion+",
+        details: "Calling numbers connected",
+      },
+    ],
+  },
+];
 
-  // Regex for password validation
-  export const passwordRegex = {
-    pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
-  };
+// Community section of Hero page
+export const items = [
+  {
+    id: 1,
+    image: "/assets/icons/support.svg",
+    title: "Community Support",
+    description:
+      "Community Support is also available for all open source users.",
+  },
+  {
+    id: 2,
+    image: "/assets/icons/github.icon.svg",
+    title: "MailBlink on GitHub",
+    description: "Track the MailBlink development changes on GitHub.",
+  },
+];
 
-  // input details for change-password page
-  export const changePasswordInput = [
-    {
-      id: "email",
-      label: "Email",
-      placeholder: "enter email",
-      type: 'text'
-    },
-    {
-      id: "newPassword",
-      label: "New password",
-      placeholder: "enter new password",
-      type: "password"
-    },
-    {
-      id: "confirmPassword",
-      label: "Confirm password",
-      placeholder: "re-enter password",
-      type: "password"
-    },
-  ];
+// Regex for email validation
+export const emailRegex = {
+  pattern:
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+};
 
-  // testimonial section
-  export const testimonials = [
-    {
-      id: 1,
-      variations: [
-        {
-          feedback: "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow1"
-        },
-        {
-          feedback: "Landify saved our time in designing my company page.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow1"
-        },
-        {
-          feedback: "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow1"
-        }
-      ]
-    },
-    {
-      id: 2,
-      variations: [
-        {
-          feedback: "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow2"
-        },
-        {
-          feedback: "Landify saved our time in designing my company page.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow2"
-        },
-        {
-          feedback: "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow2"
-        }
-      ]
-    },
-    {
-      id: 3,
-      variations: [
-        {
-          feedback: "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow3"
-        },
-        {
-          feedback: "Landify saved our time in designing my company page.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow3"
-        },
-        {
-          feedback: "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow3"
-        }
-      ]
-    },
-    {
-      id: 4,
-      variations: [
-        {
-          feedback: "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow4"
-        },
-        {
-          feedback: "Landify saved our time in designing my company page.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow4"
-        },
-        {
-          feedback: "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow4"
-        }
-      ]
-    },
-    {
-      id: 5,
-      variations: [
-        {
-          feedback: "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow5"
-        },
-        {
-          feedback: "Landify saved our time in designing my company page.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow5"
-        },
-        {
-          feedback: "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
-          image : "/assets/images/display_pic.jpeg",
-          name: "Floyd Miles",
-          position: "Co-Founder, BookMyShow5"
-        }
-      ]
-    }
-  ]
+// Regex for password validation
+export const passwordRegex = {
+  pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+};
 
-  // constants for notification page
-  export const webNotifications = [
-    {
-        id: "new-subscriber",
-        label: "New subscriber"
-    },
-    {
-        id: "post-like",
-        label: "Post like"
-    },
-    {
-        id: "you-followed",
-        label: "Someone you followed posted"
-    },
-    {
-        id: "campaign-sent",
-        label: "Campaigns sent out"
-    },
-    {
-        id: "campaign-delivered",
-        label: " Campaign delivery"
-    },
-]
+// input details for change-password page
+export const changePasswordInput = [
+  {
+    id: "email",
+    label: "Email",
+    placeholder: "enter email",
+    type: "text",
+  },
+  {
+    id: "newPassword",
+    label: "New password",
+    placeholder: "enter new password",
+    type: "password",
+  },
+  {
+    id: "confirmPassword",
+    label: "Confirm password",
+    placeholder: "re-enter password",
+    type: "password",
+  },
+];
+
+// testimonial section
+export const testimonials = [
+  {
+    id: 1,
+    variations: [
+      {
+        feedback:
+          "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow1",
+      },
+      {
+        feedback: "Landify saved our time in designing my company page.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow1",
+      },
+      {
+        feedback:
+          "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow1",
+      },
+    ],
+  },
+  {
+    id: 2,
+    variations: [
+      {
+        feedback:
+          "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow2",
+      },
+      {
+        feedback: "Landify saved our time in designing my company page.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow2",
+      },
+      {
+        feedback:
+          "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow2",
+      },
+    ],
+  },
+  {
+    id: 3,
+    variations: [
+      {
+        feedback:
+          "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow3",
+      },
+      {
+        feedback: "Landify saved our time in designing my company page.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow3",
+      },
+      {
+        feedback:
+          "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow3",
+      },
+    ],
+  },
+  {
+    id: 4,
+    variations: [
+      {
+        feedback:
+          "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow4",
+      },
+      {
+        feedback: "Landify saved our time in designing my company page.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow4",
+      },
+      {
+        feedback:
+          "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow4",
+      },
+    ],
+  },
+  {
+    id: 5,
+    variations: [
+      {
+        feedback:
+          "I was looking for a landing page UI Kit. Landify is one of the best landing page UI kit I have come across. It’s so flexible, well organised and easily editable.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow5",
+      },
+      {
+        feedback: "Landify saved our time in designing my company page.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow5",
+      },
+      {
+        feedback:
+          "I used landify and created a landing page for my startup within a week. The Landify UI Kit is simple and highly intuitive, so anyone can use it.",
+        image: "/assets/images/display_pic.jpeg",
+        name: "Floyd Miles",
+        position: "Co-Founder, BookMyShow5",
+      },
+    ],
+  },
+];
+
+// constants for notification page
+export const webNotifications = [
+  {
+    id: "new-subscriber",
+    label: "New subscriber",
+  },
+  {
+    id: "post-like",
+    label: "Post like",
+  },
+  {
+    id: "you-followed",
+    label: "Someone you followed posted",
+  },
+  {
+    id: "campaign-sent",
+    label: "Campaigns sent out",
+  },
+  {
+    id: "campaign-delivered",
+    label: " Campaign delivery",
+  },
+];
 
 export const mailNotifications = [
-    {
-        title: "Weekly newsletter",
-        label: "A small text about what the newsletters might contain"
-    },
-    {
-        title: "Account summary",
-        label: "A small text about what the newsletters might contain"
-    },
-]
+  {
+    title: "Weekly newsletter",
+    label: "A small text about what the newsletters might contain",
+  },
+  {
+    title: "Account summary",
+    label: "A small text about what the newsletters might contain",
+  },
+];
 
 // accounts page
 export const passwordInputs = [
