bb9c226889ab49bd302727acbd6aea6ab5539230
Chris Awoke <uchechristian27@gmail.com>
2023-12-26 21:26:17 +0100

    Update SignBtn.jsx

--- components/SignBtn.jsx
+++ components/SignBtn.jsx
@@ -1,12 +1,22 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const SignBtn = () => {
   return (
-    <div className='block md:flex items-center space-y-6 md:space-y-0 mx-auto md:flex-row space-x-3 md:space-x-6'>
-        <Link href='#' className="text-white bg-navyBlue rounded-[24px] py-2 px-6 font-medium text-sm border border-navyBlue hover:text-navyBlue hover:bg-white hover:border hover:border-navyBlue duration-200">Sign In</Link>
-        <Link href='#' className="text-navyblue bg-transparent rounded-[24px] py-2 px-6 font-medium text-sm border border-transparent hover:border-navyBlue duration-200">Sign Up</Link>
+    <div className="block md:flex items-center space-y-6 md:space-y-0 mx-auto md:flex-row space-x-3 md:space-x-6">
+      <Link
+        href="#"
+        className="text-white bg-navyBlue rounded-[24px] py-2 px-6 font-medium text-sm border border-navyBlue hover:text-navyBlue hover:bg-white hover:border hover:border-navyBlue duration-200"
+      >
+        Sign In
+      </Link>
+      <Link
+        href="#"
+        className="text-navyblue bg-transparent rounded-[24px] py-2 px-6 font-medium text-sm border border-transparent hover:border-navyBlue duration-200"
+      >
+        Login
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default SignBtn
+export default SignBtn;
