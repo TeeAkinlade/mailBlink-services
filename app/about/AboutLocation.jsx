1f4fd4910080a1679e111aa758705650a6f3a9b3
elijah <elijahndenwa19@gmail.com>
2024-01-12 14:14:15 +0100

    Edited About-Location Section of the About Page

--- app/about/AboutLocation.jsx
+++ app/about/AboutLocation.jsx
@@ -1,4 +1,4 @@
-import Image from './Image';
+import Image from "next/image";
 import { FiPhoneCall } from "react-icons/fi";
 import { aboutData } from "@/constants";
 import { BsEnvelope } from "react-icons/bs";
@@ -6,7 +6,7 @@ import { CiLocationOn } from "react-icons/ci";
 
 const AboutLocation = () => {
   return (
-     <section className="mt-10 bg-blue-50 px-4 py-10 font-poppins text-ui_primary md:px-20 md:py-[100px] lg:px-40">
+     <section className="mt-10 bg-white px-4 py-10 font-poppins text-ui_primary md:px-20 md:py-[100px] lg:px-40">
       <h1 className="text-center text-2xl font-extrabold md:text-4xl">
      Locations    
        </h1>
@@ -15,35 +15,26 @@ const AboutLocation = () => {
           {aboutData.map((item, index) => (
             <div
               key={index}
-              className="rounded-lg overflow-hidden  px-5 py-5 text-center"
+              className="rounded-3xl overflow-hidden  px-5 py-5 text-center border-2 border-blue-100 transition-transform hover:scale-105 hover:border-white hover:bg-white"
             >
               <div className="flex items-center justify-center ">
                <Image
               alt=""
-              width="200"
+              width="900"
               height="200"
               src={item.image}
                />     
-                
               </div>
-              <h2 className="flex items-center text-sm md:text-base my-2">
-                  <FiPhoneCall className="text-navyBlue" size={24} />
-                  <span className="ml-4 text-[#565656] text-md mt-3 font-bold md:mt-5 md:text-lg">
-                    {item.phone}
-                  </span>
-              </h2>
-              <h2 className="flex items-center text-sm md:text-base my-2">
-                  <BsEnvelope className="text-navyBlue" size={24} />
-                  <span className="ml-4 text-[#565656] text-md mt-3 font-bold md:mt-5 md:text-lg">
-                   {item.contact}
-                  </span>
-              </h2>
-              <h2 className="flex items-center text-sm md:text-base my-2">
-                  <CiLocationOn className="text-navyBlue" size={24} />
-                  <span className="ml-4 text-[#565656] text-md mt-3 font-bold md:mt-5 md:text-lg">
-                   {item.address}
-                  </span>
-              </h2>
+               <h3 className="text-md mt-3 font-bold md:mt-5 md:text-lg">
+                {item.title}
+                </h3>
+                <h3 className="text-md mt-3 font-bold md:mt-5 md:text-lg">
+                {item.phone}
+                  </h3>
+                 <h3 className="text-md mt-3 font-bold md:mt-5 md:text-lg">
+                {item.contact}
+               </h3>
+              <p className="text-sm md:text-base">{item.address}</p>
             </div>
           ))}
         </div>
