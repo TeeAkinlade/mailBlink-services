6ce65e0b39f134d1d24fab454e74ca724a6a7f5e
Chris Awoke <uchechristian27@gmail.com>
2024-01-13 19:53:41 +0100

    Latest update

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
-import Footer from './home/footer/Footer';
-import Nav from './home/nav/Nav';
-import '/styles/globals.css';
+import Footer from "./home/footer/Footer";
+import Nav from "./home/nav/Nav";
+import "/styles/globals.css";
 
 export const metadata = {
-	title: 'MailBlink - One-stop Email Marketing Services',
-	description: 'Email and SMS Marketing Services',
+  title: "MailBlink - One-stop Email Marketing Services",
+  description: "Email and SMS Marketing Services",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang='en'>
-			<body className='font-poppins min-h-screen min-w-screen bg-secondary'>
-				{/* <Nav /> */}
-				{children}
-				{/* <Footer /> */}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className="font-poppins min-h-screen min-w-screen bg-secondary">
+        <Nav />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
