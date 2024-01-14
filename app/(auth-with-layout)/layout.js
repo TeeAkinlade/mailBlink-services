3ba754d9fa16cd10a561eb9fb6a748062a420d71
Chris Awoke <uchechristian27@gmail.com>
2024-01-14 12:24:11 +0100

    Create layout.js

---
+++ app/(auth-with-layout)/layout.js
@@ -0,0 +1,9 @@
+export default function AuthLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="font-poppins min-h-screen min-w-screen bg-secondary">
+        {children}
+      </body>
+    </html>
+  );
+}
