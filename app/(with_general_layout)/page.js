c2b9af9732076fc3c13ff58dc89b04c5cb8a9df4
Chris Awoke <uchechristian27@gmail.com>
2024-01-14 12:33:39 +0100

    Create page.js

---
+++ app/(with_general_layout)/page.js
@@ -0,0 +1,17 @@
+import Campaign from "./home/Campaign";
+import Subscribe from "./home/Subscribe";
+import Services from "./home/Services";
+import Hero from "./home/Hero";
+import About from "./home/About";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Campaign />
+      <Subscribe />
+      <Services />
+    </main>
+  );
+}
