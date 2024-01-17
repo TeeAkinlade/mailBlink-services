3d1130ec2df553585604263ed705fc2023e1f5ab
Chris Awoke <uchechristian27@gmail.com>
2024-01-17 15:55:24 +0100

    Update layout.js

--- app/(auth-with-layout)/dashboard/layout.js
+++ app/(auth-with-layout)/dashboard/layout.js
@@ -1,5 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
+
+import { useEffect, useState, useRef } from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import { useCurrentUser } from "../currentUser";
@@ -7,54 +8,50 @@ import { useRouter } from "next/navigation";
 import Spinner from "@/components/Spinner";
 import Footer from "./components/Footer";
 import "/styles/globals.css";
+
 export default function DashboardLayout({ children }) {
   const { user, loading } = useCurrentUser();
   const router = useRouter();
   const [authorized, setAuthorized] = useState(false);
 
-  //   console.log(user);
-  let timer;
+  const timerRef = useRef(null);
+
   useEffect(() => {
-    timer = setTimeout(() => {
+    timerRef.current = setTimeout(() => {
       return null;
     }, 10000);
 
     if (user?.aud === "authenticated") {
       console.log("authorized");
       setAuthorized(true);
-
-      // 10 seconds in milliseconds
     }
 
     return () => {
       // Clear the timer if the component unmounts before the delay
-      clearTimeout(timer);
+      clearTimeout(timerRef.current);
     };
-  }, [timer]);
+  }, [user]);
 
-  // console.log(timer);
   // protected route logic using a timer
-  if (!authorized && timer === 10000) {
-    return router.push("/auth/login");
-  }
+  useEffect(() => {
+    if (!authorized && timerRef.current === 10000) {
+      router.push("/auth/login");
+    }
+  }, [authorized, router]);
+
   return (
     <>
       {/* Sidebar component remains static above all elements on the page */}
       <main className="flex overflow-hidden h-screen w-screen dark:bg-dark_bg dark:text-dark_text ">
-        {/* <Sidebar /> */}
         <Sidebar />
         {/* Main Dashboard display area */}
         <div className="flex flex-col overflow-x-hidden justify-between gap-4 md:gap-0 w-full h-full overflow-y-auto ">
-          {/* Navbar COMPONENT goes here */}
-
           <Navbar />
-          {/* When routing begins we would use the <Outlet/> functionality to render different pages */}
           {loading ? (
             <Spinner />
           ) : (
             <div className="flex flex-col mb-6">{children}</div>
           )}
-
           <div>
             <Footer />
           </div>
