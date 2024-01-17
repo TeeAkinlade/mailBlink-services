162437084f02146d0591657f88fd396938f57638
Chris Awoke <uchechristian27@gmail.com>
2024-01-17 12:21:39 +0100

    Update currentUser.js

--- app/(auth-with-layout)/currentUser.js
+++ app/(auth-with-layout)/currentUser.js
@@ -8,7 +8,7 @@ export const useCurrentUser = () => {
 	const [user, setUser] = useState(null);
 	const [loading, setLoading] = useState(true);
 
-<<<<<<< HEAD
+
 	useEffect(() => {
 		const fetchUser = async () => {
 			try {
@@ -29,29 +29,6 @@ export const useCurrentUser = () => {
 				setLoading(false);
 			}
 		};
-=======
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const {
-          data: { user },
-          error,
-        } = await supabase.auth.getUser();
-        // console.log("hook",user);
-        if (error) {
-          console.log(error.message);
-        } else {
-          // console.log(user);
-          setUser(user);
-        }
-      } catch (error) {
-        console.error("Error fetching user:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
->>>>>>> 194bee6c0d5043c6c109e3bf3ee620a6af25b6c4
-
 		fetchUser();
 	}, []);
 
