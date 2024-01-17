529ab5e5b0e9286ecb10ee9a9b02857978f710cf
Chris Awoke <uchechristian27@gmail.com>
2024-01-17 17:18:57 +0100

    Update SMSList.jsx

--- app/(auth-with-layout)/dashboard/sms/SMSList.jsx
+++ app/(auth-with-layout)/dashboard/sms/SMSList.jsx
@@ -18,7 +18,7 @@ export const SMSList = ({ smsCampaigns, getCampaigns }) => {
 
   useEffect(() => {
     getCampaigns();
-  }, []);
+  }, [getCampaigns]);
 
   useEffect(() => {
     console.log(smsCampaigns);
